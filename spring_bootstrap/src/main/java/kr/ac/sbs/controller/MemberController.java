b2f4a0b00a54e19bd603ab2d61e7fc3341a4638e
SBS <g4826@naver.com>
2023-02-13 13:53:48 +0900

    1:50 / class over

--- spring_bootstrap/src/main/java/kr/ac/sbs/controller/MemberController.java
+++ spring_bootstrap/src/main/java/kr/ac/sbs/controller/MemberController.java
@@ -7,10 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jsp.command.SearchCriteria;
+import com.jsp.dto.MemberVO;
 import com.jsp.service.MemberService;
+
+import kr.ac.sbs.command.MemberRegistCommand;
 
 @Controller
 @RequestMapping("/member")
@@ -28,6 +34,31 @@ public class MemberController {
 		request.setAttribute("dataMap", dataMap);
 	}
 	
+	@GetMapping("/registForm")
+	public String registForm() {
+		String url = "/member/regist";
+		return url;
+	}
+	
+	@PostMapping("/regist")
+	public String regist(MemberRegistCommand memberReq) throws Exception{
+		String url = "/member/regist_success";
+		
+		MemberVO member = memberReq.toMemberVO();
+		memberService.regist(member);
+		
+		return url;
+	}
+	
+	@Resource(name="picturePath")
+	private String picturePath;
+	
+	@PostMapping(value="/picture", produces="text/plain;charset=utf-8")
+	public String pictureUpload(@RequestParam("pictureFile")MultipartFile multi, String oldPicture) throws Exception{
+		
+		
+		return null;
+	}
 	
 	
 	
