905a0073542a7b9d2a8605b9dfd4ccb1d7369cec
SBS <g4826@naver.com>
2023-03-02 11:37:47 +0900

    spring_bootstrap: study 0302/ security

--- spring_bootstrap/src/main/java/kr/ac/sbs/controller/CommonController.java
+++ spring_bootstrap/src/main/java/kr/ac/sbs/controller/CommonController.java
@@ -1,17 +1,14 @@
 package kr.ac.sbs.controller;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jsp.service.MemberService;
-
-import kr.ac.sbs.exception.InvalidPasswordException;
-import kr.ac.sbs.exception.NotFoundIdException;
 
 @Controller
 public class CommonController {
@@ -30,28 +27,51 @@ public class CommonController {
 	}
 	
 	@GetMapping("/common/loginForm")
-	public void loginForm() {}
-	
-	
-	@PostMapping("/common/login")
-	public String login(String id, String pwd, HttpSession session, RedirectAttributes rttr) throws Exception {
-		String url = "redirect:/index.do";
-		try {
-			loginMemberService.login(id, pwd);
-			session.setAttribute("loginUser", loginMemberService.getMember(id));
-		} catch(NotFoundIdException | InvalidPasswordException e) {
-			url = "redirect:/common/loginForm.do";
-			rttr.addFlashAttribute("message", e.getMessage());
-			rttr.addFlashAttribute("id", id);
+	public String loginForm(@RequestParam(defaultValue="") String error, HttpServletResponse response) {
+		String url = "/common/loginForm";
+		
+		if(error.equals("-1")) {
+			response.setStatus(302);
 		}
 		return url;
 	}
 	
-	@GetMapping("/common/logout")
-	public String logout(HttpSession session) {
-		String url = "redirect:/";
-		session.invalidate();
+	@GetMapping("/security/accessDenied")
+	public void accessDenied() {}
+	
+	@GetMapping("/common/loginTimeOut")
+	public String loginTimeOut(Model model) throws Exception{
+		
+		String url = "/security/sessionOut";
+		model.addAttribute("message","세션이 만료되었습니다.\\n다시 로그인 하세요!");
 		return url;
 	}
+	
+	
+//	@PostMapping("/common/login")
+//	public String login(String id, String pwd, HttpSession session, RedirectAttributes rttr) throws Exception {
+//		String url = "redirect:/index.do";
+//		try {
+//			loginMemberService.login(id, pwd);
+//			session.setAttribute("loginUser", loginMemberService.getMember(id));
+//		} catch(NotFoundIdException | InvalidPasswordException e) {
+//			url = "redirect:/common/loginForm.do";
+//			rttr.addFlashAttribute("message", e.getMessage());
+//			rttr.addFlashAttribute("id", id);
+//		}
+//		return url;
+//	}
+	
+//	@GetMapping("/common/logout")
+//	public String logout(HttpSession session) {
+//		String url = "redirect:/";
+//		session.invalidate();
+//		return url;
+//	}
+	
+	
+	
+	
+	
 
 }
